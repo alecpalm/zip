736117fd22dec809fbb87062ef993617e9800b3f
alecpalm <97771933+alecpalm@users.noreply.github.com>
2023-05-13 22:44:25 -0700

    Create fetchAll.js

---
+++ functions/fetchAll.js
@@ -0,0 +1,17 @@
+const faunadb = require('faunadb');
+
+const client = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET });
+const q = faunadb.query;
+
+exports.handler = async function(event, context) {
+  if (event.httpMethod !== 'GET') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  try {
+    const urls = await client.query(q.Map(q.Paginate(q.Match(q.Index('all_urls'))), q.Lambda('x', q.Get('x'))));
+    return { statusCode: 200, body: JSON.stringify(urls) };
+  } catch (error) {
+    return { statusCode: 500, body: 'An error occurred: ' + error.toString() };
+  }
+};

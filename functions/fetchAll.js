c356b6c114ebc4441175e15433c1d83fdfd1c804
alecpalm <97771933+alecpalm@users.noreply.github.com>
2023-05-13 22:57:14 -0700

    Update fetchAll.js

--- functions/fetchAll.js
+++ functions/fetchAll.js
@@ -10,8 +10,10 @@ exports.handler = async function(event, context) {
 
   try {
     const urls = await client.query(q.Map(q.Paginate(q.Match(q.Index('all_urls'))), q.Lambda('x', q.Get('x'))));
+    console.log('URLs fetched successfully:', urls);  // Debugging line
     return { statusCode: 200, body: JSON.stringify(urls) };
   } catch (error) {
+    console.error('An error occurred:', error);  // Debugging line
     return { statusCode: 500, body: 'An error occurred: ' + error.toString() };
   }
 };

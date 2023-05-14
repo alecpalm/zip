2b905cd014c868db809018f09c38b42456a37440
alecpalm <97771933+alecpalm@users.noreply.github.com>
2023-05-13 21:50:47 -0700

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -4,11 +4,15 @@ const client = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET })
 const q = faunadb.query;
 
 exports.handler = async function(event, context) {
-  const short = event.path.substring(1);
+  const short = event.path.substring(3);
+  console.log('Short URL:', short); // Debugging: log the short URL
+
   const query = q.Get(q.Match(q.Index('urls_by_short'), short));
 
   try {
     const result = await client.query(query);
+    console.log('Query result:', result); // Debugging: log the query result
+
     return {
       statusCode: 302,
       headers: {
@@ -17,6 +21,7 @@ exports.handler = async function(event, context) {
       body: ''
     };
   } catch (error) {
+    console.error('Error:', error); // Debugging: log the error
     return { statusCode: 404, body: 'OOPS' };
   }
 };
